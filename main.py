e24e050075ef21913f19400fa41df6107cb477bd
abhinavSingh <abhi.anu003@gmail.com>
2020-12-03 17:57:56 +0530

    error sorting going on

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import os
+import time, keyboard, os
 
-os.system("script.py")
+timeout = 5
+id = 0
+start_time = time.time()
+
+print("Press DEL to enter Setup..........")
+
+while True:
+    
+    if keyboard.is_pressed("del"):
+        os.system("script.py")
+        break
+    if id == 0 and (time.time() - start_time) > timeout:
+        #os.system("startx")
+        print("startx")
+        break
+    #time.sleep(1)
+    #os.system("cls")
+
+
--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("Hello")
+n = int(input(("Enter your choice : "))
+
+if(n==1):
+    print("1")
+elif(n==2):
+    print("2")
+else:
+    print("Else")
