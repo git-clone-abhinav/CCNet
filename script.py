c8f51bcbe3d98b425c87597a11350528fd715a79
abhinavSingh <abhi.anu003@gmail.com>
2020-12-03 18:06:56 +0530

    final commit

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
-n = int(input(("Enter your choice : "))
 
-if(n==1):
-    print("1")
-elif(n==2):
-    print("2")
-else:
-    print("Else")
+while True:
+    n=int(input("Enter your choice :"))
+
+    if (n==1):
+        print("1")
+    elif (n==2):
+        print("2")
+    elif (n==3):
+        print("3")
+    else :
+        print("else")
